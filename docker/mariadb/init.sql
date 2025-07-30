1a9029af3afeecff3176ade1736bda10038bf300
Aleksandr Moskalev <alexmasz@yandex.ru>
2025-07-30 23:09:54 +0300

    files modified

--- docker/mariadb/init.sql
+++ docker/mariadb/init.sql
@@ -1,15 +1,7 @@
-CREATE DATABASE IF NOT EXISTS flask_app_db;
-CREATE USER 'flask_app_user'@'%' IDENTIFIED BY 'flask_app_password';
-
-USE flask_app_db;
-
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(80) NOT NULL UNIQUE,
     email VARCHAR(120) NOT NULL UNIQUE
 );
 
-INSERT INTO users (username, email) VALUES('flask-mariadb', 'flask-mariadb@username.com')
-
-GRANT ALL PRIVILEGES ON flask_app_db.* TO 'flask_app_user'@'%';
-FLUSH PRIVILEGES;
+INSERT INTO users (username, email) VALUES('username', 'email@example.com');
